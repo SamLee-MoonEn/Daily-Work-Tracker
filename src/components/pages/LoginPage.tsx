ce2036b9dc32934591a1c4fc63d6398ae7d019e3
SamLee <goha2010@naver.com>
2023-07-14 16:52:27 +0900

    MS 로그인 기능 추가

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,8 +1,49 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
 import { styled } from "styled-components";
 import tw from "twin.macro";
 
+import microsoftLoginIcon from "../../assets/ms-symbollockup_signin_light.png";
+import { createNewAccount, handleMicrosoftLogin } from "../../api/firebaseAPI";
+import { userInfoState } from "../../store/userInfo";
+
 export default function LoginPage() {
-  return <MainContainer>로그인</MainContainer>;
+  const navigate = useNavigate();
+  const [user, setUser] = useRecoilState(userInfoState);
+
+  const handleLogin = async () => {
+    try {
+      const userInfo = await handleMicrosoftLogin();
+      setUser({
+        displayName: userInfo?.displayName as string,
+        email: userInfo?.email as string,
+        uid: userInfo?.uid as string,
+      });
+      await createNewAccount(user);
+      localStorage.setItem("isLogin", "true");
+      navigate("/register");
+    } catch (error) {
+      console.error(`[Error] handleLogin ${new Date()}: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    const loginCheck = localStorage.getItem("isLogin");
+    if (localStorage.getItem("USER_UID") && loginCheck) {
+      navigate("/register");
+    }
+  });
+
+  return (
+    <MainContainer>
+      <div className="mx-auto mt-96 w-fit">
+        <button onClick={handleLogin}>
+          <img src={microsoftLoginIcon} />
+        </button>
+      </div>
+    </MainContainer>
+  );
 }
 
 const MainContainer = styled.div`
