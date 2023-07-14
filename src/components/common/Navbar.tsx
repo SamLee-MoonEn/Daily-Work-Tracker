6070d2fa8d34cddf2fcaeb1a61828e68be70e713
SamLee <goha2010@naver.com>
2023-07-14 16:35:06 +0900

    Navbar에 로고 추가 및 로고 추가 시 초기 화면으로 가도록 수정.

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 
+import kohyoungIcon from "../../assets/kohyoung_icon.png";
+
 export default function Navbar() {
   return (
     <nav className="relative">
       <div className="h-screen w-1/12 fixed top-0 left-0 bg-primary">
-        <div className="flex flex-col mt-20 text-sm xs:text-lg">
+        <div className="flex flex-col mt-10 text-sm xs:text-lg">
+          <Link to="/" className="flex justify-center mb-10">
+            <img src={kohyoungIcon} className="w-10" />
+          </Link>
           <Link
             to="/register"
             className="p-4 text-white hover:bg-gray-300 hover:text-black transition-all text-center"
