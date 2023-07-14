8c911061283c5e6a2dd057e3dfbeec1c4aa1d24e
SamLee <goha2010@naver.com>
2023-07-14 16:55:15 +0900

    Recoil로 전역 상태 관리 추가.

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/common/Navbar";
 import LoginPage from "./components/pages/LoginPage";
 import WorkRegisterPage from "./components/pages/WorkRegisterPage";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LoginPage />}></Route>
-        <Route path="/register" element={<WorkRegisterPage />}></Route>
-      </Routes>
+      <RecoilRoot>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/register" element={<WorkRegisterPage />}></Route>
+        </Routes>
+      </RecoilRoot>
     </BrowserRouter>
   );
 }
