f7723b39345a3e7e5410d7af8c56a38feb4f826b
SamLee <goha2010@naver.com>
2023-07-14 16:53:31 +0900

    상수를 추가할 파일 추가

---
+++ src/components/constant/constant.ts
@@ -0,0 +1,22 @@
+export const regionOptions = [
+  "HQ",
+  "KYA",
+  "KYC",
+  "KYE",
+  "KYSEA",
+  "JKY",
+  "Intel",
+];
+export const typeOptions = [
+  "HQ System",
+  "Cooperation",
+  "Manual",
+  "Setting",
+  "Parts",
+  "Remote",
+  "Resolution",
+  "Back up image",
+  "Spec",
+  "Sample Test",
+  "ETC",
+];
