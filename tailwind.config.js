ba6766e2e595dbd12fab10485ea9081eaae31824
SamLee <goha2010@naver.com>
2023-07-18 17:41:45 +0900

    tailwind 화면 사이즈 설정.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["/index.html", "./**/*.{ts,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1025px",
+      xl: "1280px",
+      xl2: "1360px",
+    },
     extend: { colors: { light: "#79A9AE" } },
     fontFamily: {
       sans: ["GmarketSansMedium"],
