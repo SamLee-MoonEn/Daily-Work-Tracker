da52cc44e52bad46f22122c93219e327130f46ee
SamLee <goha2010@naver.com>
2023-07-18 17:44:32 +0900

    표에서 표시할 Date 포맷을 변경하는 함수 추가

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -2,3 +2,10 @@ export const makeUniqueId = (uid: string) => {
   const random = Math.floor(Math.random() * 100000000);
   return `${uid}-${random}${new Date().getTime()}`;
 };
+
+export const makeFormattedDate = (date: Date) => {
+  const tempDate = new Date(date);
+  return `${tempDate.getFullYear()}년 ${
+    tempDate.getMonth() + 1
+  }월 ${tempDate.getDate()}일`;
+};
