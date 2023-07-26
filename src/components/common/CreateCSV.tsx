bbc0baf501659dacbe2a2a1adb3c260c9e094eb5
SamLee <goha2010@naver.com>
2023-07-26 14:51:05 +0900

    예시 CSV 다운로드를 위한 Component 생성

---
+++ src/components/common/CreateCSV.tsx
@@ -0,0 +1,42 @@
+import { CSVLink } from "react-csv";
+
+export default function CreateCSVButton() {
+  const headers = [
+    { label: "요청지역", key: "region" },
+    { label: "Customer", key: "customer" },
+    { label: "요청사항", key: "content" },
+    { label: "작업일자", key: "selectedDate" },
+    { label: "HQ Owner", key: "owner" },
+    { label: "유형", key: "type" },
+    { label: "소요시간(min)", key: "timeTaken" },
+    { label: "Remark", key: "remark" },
+  ];
+  const exampleData = [
+    {
+      region: "예시",
+      customer: "예시",
+      content: "",
+      selectedDate: "",
+      owner: "",
+      type: "예시",
+      timeTaken: "",
+      remark: "",
+    },
+  ];
+  return (
+    <CSVLink
+      data={exampleData}
+      headers={headers}
+      onClick={() => {
+        if (confirm("파일을 다운로드 받겠습니까?")) {
+          return true;
+        } else {
+          return false;
+        }
+      }}
+      filename="example"
+    >
+      예시 파일 다운로드
+    </CSVLink>
+  );
+}
