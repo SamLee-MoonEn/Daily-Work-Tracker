84802380894aae522dd9675a33547a2c515e649a
SamLee <goha2010@naver.com>
2023-09-05 17:54:12 +0900

    새로 만든 Power BI Page로 교체 및 스타일 수정.

--- src/components/workAnalysis/WorkAnalysis.tsx
+++ src/components/workAnalysis/WorkAnalysis.tsx
@@ -6,8 +6,8 @@ export default function WorkAnalysis() {
     <MainContainer>
       <iframe
         title="Non CRM 업무 정리_2022.04.01"
-        className="w-full h-[95vh]"
-        src="https://app.powerbi.com/reportEmbed?reportId=46185582-cf5b-4093-824c-dcfe443f022f&autoAuth=true&ctid=676ac0b7-276a-4e65-82ad-f13e2c55cf8c"
+        className="w-full h-[93vh] mt-2"
+        src="https://app.powerbi.com/reportEmbed?reportId=b21f8186-bc32-4d03-bafc-05435aaf0ff9&autoAuth=true&ctid=676ac0b7-276a-4e65-82ad-f13e2c55cf8c"
         allowFullScreen={true}
       ></iframe>
     </MainContainer>
