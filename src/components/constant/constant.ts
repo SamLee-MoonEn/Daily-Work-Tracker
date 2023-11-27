085a8d9239db1ec5b64b60e56622cb3c4aa32af3
SamLee <goha2010@naver.com>
2023-11-27 17:12:46 +0900

    옵션 변경. 옵션에 설명이 필요한 부분이 있는 경우 설정

--- src/components/constant/constant.ts
+++ src/components/constant/constant.ts
@@ -8,15 +8,25 @@ export const REGIONS_OPTION = [
   "Intel",
 ];
 export const TYPE_OPTION = [
-  "HQ System",
-  "Cooperation",
-  "Manual",
-  "Setting",
   "Parts",
+  "Manual",
   "Remote",
-  "Resolution",
   "Back up image",
   "Spec",
-  "Sample Test",
+  "Sample Test/Buyoff",
+  "Cooperation",
+  "HQ System",
   "ETC",
+];
+
+export const TYPE_DESCRIPTION = [
+  "Parts: 파트와 관련된 업무 등록용. 파트 찾기, 파트 설치 메뉴얼 작성 등",
+  "Manual: 매뉴얼 작성, 매뉴얼 검색 등 관련 업무 등록용. 업무 완료 후 Remark에 메뉴얼 작성 혹은 검색인지 명확히 등록 필요.",
+  "Remote: 원격 관련 업무 등록용.",
+  "Backup image: 지사에 Backup Image 전달 관련 업무 등록용.",
+  "Spec: 장비, Vision Spec 등의 전달 관련 업무 등록용.",
+  "Sample/Buyoff: Sample Test, Buyoff 관련 업무 등록용.",
+  "Cooperation: 타 부서와의 연계 업무 등록용. PM/UX 인터뷰 등",
+  "HQ System: CRM, 업무 지원 툴(PSS, Non CRM) 개선 등의 관련 업무 등록용.",
+  "ETC: 상기 업무 외 기타 업무 등록용. Remark에 명확한 업무 기록 필요.",
 ];
