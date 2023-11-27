8092a81ab24b783df592f23c88bb2dc7f876e368
SamLee <goha2010@naver.com>
2023-11-27 17:08:19 +0900

    라이브러리 Update

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,16 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          "https://login.microsoftonline.com/676ac0b7-276a-4e65-82ad-f13e2c55cf8c/oauth2/v2.0/token",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
